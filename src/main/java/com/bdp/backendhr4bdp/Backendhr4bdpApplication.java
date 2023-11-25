7b13f5b34839ce10b9e596c1860a165b139db77c
shahmiebdp <152031688+shahmiebdp@users.noreply.github.com>
2023-11-26 04:06:31 +0800

    preliminary kick start commit
    let's gooooo!

---
+++ src/main/java/com/bdp/backendhr4bdp/Backendhr4bdpApplication.java
@@ -0,0 +1,13 @@
+package com.bdp.backendhr4bdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backendhr4bdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backendhr4bdpApplication.class, args);
+	}
+
+}
